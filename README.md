65f480bb00dcce3afd6b0cdc86a7ef9a45d076cc
Edhyjox <edhjyox@gmail.com>
2021-02-02 01:23:50 +0100

    set version

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Impauto
+###version 1.0.1
 *Make your life easier with automated inputs*
 
 ## Installation
--- impauto/__init__.py
+++ impauto/__init__.py
@@ -1,7 +1,7 @@
 import builtins
 
 author = "Edhyjox"
-version = "1.0.0"
+version = "1.0.1"
 
 
 class Automated:
