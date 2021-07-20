69ae912dc41700b4a31b09113d43e6da35c30b26
Yohann Boniface <edhyjox@gmail.com>
2021-07-20 23:29:13 +0200

    Adding typing to __init__.py

--- impauto/__init__.py
+++ impauto/__init__.py
@@ -1,33 +1,35 @@
 """A python package to handle automatically input for faster development."""
 import builtins
 
-author = "Sigmanificient"
-version = "1.0.1"
+from typing import Callable, Optional, Generator, Tuple
+
+author: str = "Sigmanificient"
+version: str = "1.0.1"
 
 
 class Automated:
 
-    def __init__(self, *args, show_message=True, forever=False):
+    def __init__(self, *args, show_message: bool = True, forever: bool = False) -> None:
         """Create a value generator and replace the builtin input."""
-        self.values = self.__generate_values(args, forever)
-        self.show_message = show_message
+        self.values: Generator = self.__generate_values(args, forever)
+        self.show_message: bool = show_message
 
-        builtins.input = self.__input__
+        builtins.input: Callable[[Optional[str]], str] = self.__input__
 
     @staticmethod
-    def __generate_values(values, forever):
-        values = tuple(map(str, values))
-        once = 1
+    def __generate_values(values, forever: bool) -> Generator:
+        values: Tuple[str] = tuple(map(str, values))
+        once: int = 1
 
         while forever or once:
             yield from values
-            once = False
+            once: int = 0
 
         raise StopIteration("No more input values given. Make sure to have enough values passed")
 
-    def __input__(self, __prompt=None):
+    def __input__(self, __prompt: Optional[str] = None) -> str:
         """Method that will replace the builtin input."""
-        value = next(self.values)
+        value: str = next(self.values)
 
         if self.show_message:
             print(f"[Automated] {__prompt}{value}")
